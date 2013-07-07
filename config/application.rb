50e2312acee9175f01bc960990f4aeb9f1c52375
Adrian Chira <adrian.chira@rocketmail.com>
2013-07-08 02:06:23 +0300

    changed application rb neded for AA on heroku

--- config/application.rb
+++ config/application.rb
@@ -66,6 +66,6 @@ module Eval
 
     # Precompile additional assets. Defaults to [application.js, application.css, non-JS/CSS]
     config.assets.precompile += ['active_admin.css.scss', 'active_admin.js'] 
-
+    config.assets.precompile += %w( active_admin.css active_admin.js active_admin/print.css )
   end
 end
