1a9ab7095e2b9e439931ad06c5993273011fde98
Adrian Chira <adrian.chira@rocketmail.com>
2013-07-22 01:10:24 +0300

    Add image to admin dashboard

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,4 +1,13 @@
 ActiveAdmin.register User do
+  index do
+    column  "Nume angajat", :name
+    column  "Marca", :marca 
+    column "Foto" do |user|
+    image_tag(user.image :tiny)
+                    end
+  
+  default_actions
+end
   form do |f|
   f.inputs do
     f.input :name, label: "Nume si prenume"
@@ -10,6 +19,7 @@ ActiveAdmin.register User do
     f.input :evaluator_id, label: "Este evaluat de:"
     f.input :marca, label: "Marca"
   end
+
   f.buttons
 end
 end
